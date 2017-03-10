6a0acede6df234cda702a074fb08ed1ae30a1eef
Kevin <kfulkerson92@gmail.com>
2017-03-09 21:46:34 -0500

    Improve type assertion logic and slice/map handling in find method.

--- src/test/main.go
+++ src/test/main.go
@@ -29,36 +29,41 @@ func main() {
 	// Print out each of the elements read from the file
 	recursivePrint("root", fileMap)
 
+	fmt.Print("\n\n")
+
 	// Search for a value in the map
-	keys := []string{"second", "second", "1"}
+	keys := []string{"third", "first", "first", "0", "five", "2"}
 	value, success := findValueInMap(keys, fileMap)
 	if success {
-		fmt.Printf("\nvalue = %s\n", value)
+		fmt.Printf("first value = %s\n", value)
+	}
+
+	// Search for another value in the map
+	keys = []string{"second", "second", "1"}
+	value, success = findValueInMap(keys, fileMap)
+	if success {
+		fmt.Printf("second value = %s\n", value)
 	}
 }
 
 // recursivePrint prints the structure of the given parsed YAML map.
-// BUG(ksf): Currently makes a lot of assumptions about how the data is structured.
 func recursivePrint(chain string, structure interface{}) {
 	localMap, isMap := structure.(map[interface{}]interface{})
 	localSlice, isSlice := structure.([]interface{})
-	localInteger, isInteger := structure.(int)
-	localString, isString := structure.(string)
 
 	switch {
 	case isMap:
 		for key, value := range localMap {
-			nextChain := chain + "_" + key.(string)
+			nextChain := chain + "_" + fmt.Sprintf("%v", key)
 			recursivePrint(nextChain, value)
 		}
 	case isSlice:
 		for i := 0; i < len(localSlice); i++ {
-			fmt.Printf("%s_value_%d:\n%v\n", chain, i, localSlice[i])
+			nextChain := chain + fmt.Sprintf("[%d]", i)
+			recursivePrint(nextChain, localSlice[i])
 		}
-	case isInteger:
-		fmt.Printf("%s_value:\n%v\n", chain, localInteger)
-	case isString:
-		fmt.Printf("%s_value:\n%v\n", chain, localString)
+	default:
+		fmt.Printf("%s_bool:\n%v\n", chain, structure)
 	}
 }
 
@@ -66,7 +71,6 @@ func recursivePrint(chain string, structure interface{}) {
 // but is also lenient toward simply an array (or even a single value), abstracted as a blank
 // interface.
 // It returns the value as a string and a flag indicating if the operation was successful.
-// BUG(ksf): Currently makes a lot of assumptions about how the data is structured.
 func findValueInMap(keys []string, mapToSearch interface{}) (value string, foundValue bool) {
 	// Default return value to be false
 	foundValue = false
@@ -75,29 +79,42 @@ func findValueInMap(keys []string, mapToSearch interface{}) (value string, found
 	for i := 0; i < len(keys); i++ {
 		localMap, isMap := localStructure.(map[interface{}]interface{})
 		localSlice, isSlice := localStructure.([]interface{})
-		localInteger, isInteger := localStructure.(int)
-		localString, isString := localStructure.(string)
 
 		switch {
 		case isMap:
 			localStructure = localMap[keys[i]]
 			if localStructure == nil {
-				foundValue = false
 				return
+			} else {
+				// If the value is not a map or slice, then decrement i to reinterpret the value
+				// using the same key
+				localMap, isMap = localStructure.(map[interface{}]interface{})
+				localSlice, isSlice = localStructure.([]interface{})
+				if !isMap && !isSlice {
+					i--
+				}
 			}
 		case isSlice:
-			keyAsInteger, err := strconv.ParseInt(keys[i], 10, 32)
+			keyAsInteger, err := strconv.Atoi(keys[i])
 			if err != nil {
-				panic("findValueInMap(keys() - Failed to convert key as an integer")
+				return
 			}
 
-			value = fmt.Sprintf("%v", localSlice[keyAsInteger])
-			foundValue = true
-		case isInteger:
-			value = strconv.Itoa(localInteger)
-			foundValue = true
-		case isString:
-			value = localString
+			if keyAsInteger <= len(localSlice) {
+				localStructure = localSlice[keyAsInteger]
+
+				// If the value is not a map or slice, then decrement i to reinterpret the value
+				// using the same key
+				localMap, isMap = localStructure.(map[interface{}]interface{})
+				localSlice, isSlice = localStructure.([]interface{})
+				if !isMap && !isSlice {
+					i--
+				}
+			} else {
+				return
+			}
+		default:
+			value = fmt.Sprintf("%v", localStructure)
 			foundValue = true
 		}
 	}
